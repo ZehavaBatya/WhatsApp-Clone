6b340c91a01cae3e219e3fcdf1200c6d8ec831a7
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-10-25 19:44:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import "./Sidebar.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's build a Mern WhatsApp clone</h1>
+
+      <Sidebar />
+      {/* Chat component */}
+    </div>
+  );
+}
+
+export default App;
